1de3cf6d84539797731353211cf2d1708e8c2f7d
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-06-25 21:20:55 -0500

    firste

---
+++ android/app/src/main/kotlin/com/groupg15/firebaseconn2g15/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.groupg15.firebaseconn2g15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:firebaseconn2g15/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
